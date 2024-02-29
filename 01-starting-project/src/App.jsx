0c6769a5d05516d7295a88b1d75ae4af23e05fcd
igormartins87 <202209131518@alunos.estacio.br>
2024-02-29 00:38:08 -0300

    MontandoaListaUsaandoProp

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,21 +1,68 @@
-function Header() {
+
+import img  from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
+
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+
+function Header()
+ {
+
+  const descricao = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src= {img} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {descricao} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
 
+function CoreConcept({img,titulo,descricao}){
+  return (
+    <li>
+      <img src = {img} alt={titulo}/>
+      <h3>{titulo}</h3>
+      <p>{descricao}</p>
+    </li>
+  )
+}
+
+
+
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id = "core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              titulo = {CORE_CONCEPTS[0].titulo}
+              descricao = {CORE_CONCEPTS[0].descricao}
+              img = {CORE_CONCEPTS[0].image} 
+
+              CORE_CONCEPTS {...CORE_CONCEPTS[1]}
+
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+           
+        </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
---
+++ 01-starting-project/src/data.js
@@ -0,0 +1,31 @@
+import componentsImg from './assets/components.png';
+import propsImg from './assets/config.png';
+import jsxImg from './assets/jsx-ui.png';
+import stateImg from './assets/state-mgmt.png';
+
+export const CORE_CONCEPTS = [
+  {
+    img: componentsImg,
+    titulo: 'Components',
+    descricao:
+      'teste 1',
+  },
+  {
+    img: jsxImg,
+    titulo: 'JSX',
+    descricao:
+      'teste2.',
+  },
+  {
+    img: propsImg,
+    titulo: 'Props',
+    descricao:
+      'teste3333333.',
+  },
+  {
+    img: stateImg,
+    titulo: 'State',
+    descricao:
+      'teste4.',
+  },
+];
