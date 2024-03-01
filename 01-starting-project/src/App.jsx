064bb39666c088e386c55135da2c297b6f9486a3
igormartins87 <202209131518@alunos.estacio.br>
2024-03-01 15:51:56 -0300

    EstruturandoProjetoReact

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,43 +1,8 @@
 
-import img  from './assets/react-core-concepts.png';
+;
 import { CORE_CONCEPTS } from './data.js';
-
-
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-
-function Header()
- {
-
-  const descricao = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src= {img} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {descricao} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({img,titulo,descricao}){
-  return (
-    <li>
-      <img src = {img} alt={titulo}/>
-      <h3>{titulo}</h3>
-      <p>{descricao}</p>
-    </li>
-  )
-}
-
+import Header from './Components/Header.jsx';
+import CoreConcept from './Components/CoreConcept.jsx';
 
 
 
