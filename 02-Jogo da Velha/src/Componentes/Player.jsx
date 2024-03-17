345d9fdd0d08698593e992d1de47f7a8a3ce552c
igormartins87 <202209131518@alunos.estacio.br>
2024-03-17 19:13:35 -0300

    BoaPraticadoHandleButtonClick

--- 02-Jogo da Velha/src/Componentes/Player.jsx
+++ 02-Jogo da Velha/src/Componentes/Player.jsx
@@ -4,15 +4,16 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditButtonClick = () => {
-    setIsEditing(true);
+    setIsEditing((editing)=>!editing)
+    
   };
 
   return (
     <li>
       {!isEditing && <span className="player-name">{name}</span>}
       <span className="player-symbol">{symbol}</span>
-      {isEditing && <input type="text" />}
-      <button onClick={handleEditButtonClick}>Editar</button>
+      {isEditing && <input type="text" required value ={name} />}
+      <button onClick={handleEditButtonClick}>{isEditing ?'Save':'Edit'}</button>
     </li>
   );
 }
