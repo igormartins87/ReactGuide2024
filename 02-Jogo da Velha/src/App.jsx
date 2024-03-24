9f71189c4afb40fa3d9cf5bde002791007abcceb
igormartins87 <202209131518@alunos.estacio.br>
2024-03-24 17:08:19 -0300

    LogicaParte2

--- 02-Jogo da Velha/src/App.jsx
+++ 02-Jogo da Velha/src/App.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
+
 import Player from "./Componentes/Player";
 import GameBoard from "./Componentes/GameBoard";
+import Log from "./Componentes/Log";
+
 
 function App() {
+  const[gameTurns,setGameTurns] =useState([])
+  const [jogadorAtivo,setJogadorAtivo] = useState('X');
+
+  function handleSelectSquare(){
+    setJogadorAtivo((curActivePlayer) => curActivePlayer ==='X' ? 'O':'X');
+    setGameTurns();
+  }
   return (
     <main>
       <div id="game-container">
         {/* Jogadores */}
 
-        <ol id="players">
-          <Player initialName="Jogador 1" symbol="X" />
-          <Player initialName="Jogador 2" symbol="0" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Jogador 1" symbol="X" isActive={jogadorAtivo ==='X'} />
+          <Player initialName="Jogador 2" symbol="0" isActive={jogadorAtivo ==='O'} />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol ={jogadorAtivo} />
       </div>
+      <Log />
     </main>
   );
 }
