955eff853c3bd80e9bc585732b40d575e1d1661c
igormartins87 <202209131518@alunos.estacio.br>
2024-05-03 13:33:18 -0300

    Fix: AdicionandoFooterReact

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,6 +2,7 @@
 import Header from "./Components/Header.jsx";
 import CoreComcepts from "./Components/CoreConcepts.jsx";
 import Examples from "./Components/Examples.jsx";
+import Footer from "./Components/footer.jsx";
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <main>
         <CoreComcepts />
         <Examples /> 
+        <Footer />
 
 
 
