c9d559cf208f3be73ed2769f5a53ea5148cb3f15
igormartins87 <202209131518@alunos.estacio.br>
2024-05-03 16:49:29 -0300

    adicionandoFooter

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header.jsx";
 import CoreComcepts from "./Components/CoreConcepts.jsx";
 import Examples from "./Components/Examples.jsx";
 import Footer from "./Components/footer.jsx";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 
--- 01-starting-project/src/Components/footer.jsx
+++ 01-starting-project/src/Components/footer.jsx
@@ -2,37 +2,38 @@
 import './Footer.css';
 
 
-export default function Footer()
-{
-
+export default function Footer() {
     return (
-        <footer className="footer">
-            <div className="footer-content">
-                <div className="footer-section">
-                    <p>E-mail: exemplo@email.com</p>
-                    <p>Telefone: (XX) XXXX-XXXX</p>
-                    <p>Endereço: Rua Exemplo, 123</p>
-                </div>
-                <div className="footer-section">
-                    <h2>Links Úteis</h2>
-                    <ul>
-                        <li><a href='#'>Currículo</a></li>
-                        <li><a href='#'>Git Hub</a></li>
-                        <li><a href='#'>Linkedin</a></li>
-                    </ul>
-                </div>
-                <div className="footer-section">
-                    <h2>Redes Sociais</h2>
-                    <ul className="social-icons">
-                        <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-                        <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-                        <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-                    </ul>
-                </div>
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="footer-section">
+            <p>E-mail: exemplo@email.com</p>
+            <p>Telefone: (XX) XXXX-XXXX</p>
+            <p>Endereço: Rua Exemplo, 123</p>
+          </div>
+          <div className="footer-section">
+            <h2>Links Úteis</h2>
+            <ul>
+              <li><a href="#">Currículo</a></li>
+              <li><a href="#">Git Hub</a></li>
+              <li><a href="#">Linkedin</a></li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h2>Redes Sociais</h2>
+            <ul className="social-icons">
+              <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+              <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+              <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+            </ul>
+          </div>
         </div>
-
-
-            </div>
-        </footer>
+        <div className="footer-bottom">
+          <p>&copy; 2024 Seu Nome. Todos os direitos reservados.</p>
+        </div>
+      </footer>
     );
-}
+  }
+  
+
+
