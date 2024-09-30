c9daeef156a60d400bc75bbe4caf8c0b2ccb317c
igormartins87 <202209131518@alunos.estacio.br>
2024-09-30 11:11:29 -0300

    ustates

--- Refs e Portal/src/components/Player.jsx
+++ Refs e Portal/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 export default function Player() {
+  const [enteredPlayer , SetEnteredPlayer] = useState(null);
+  const [submitted, setSubimiteed] = useState(false);
+
+  function handleChange(event){
+    setSubimiteed(false)
+    SetEnteredPlayer(event.target.value);
+  }
+
+function handleClick(){
+  setSubimiteed(true);
+
+}
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>BemVindo { submitted ? enteredPlayer: 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayer} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
