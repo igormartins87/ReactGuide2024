17de2083e1f7174751c325bac8b6d419e80d28e0
igormartins87 <202209131518@alunos.estacio.br>
2024-11-22 12:08:44 -0300

    SlideBar

---
+++ Projeto de Gestao/Componentes/ProjectsSidebar.jsx
@@ -0,0 +1,13 @@
+export default function ProjectSideBar(){
+    return <aside>
+        <h2>Seu Projetos</h2>
+        <div>
+            <button>
+                Adicionar Projetos
+            </button>
+        </div>
+        <ul>
+            
+        </ul>
+    </aside>
+}
--- Projeto de Gestao/src/App.jsx
+++ Projeto de Gestao/src/App.jsx
@@ -1,8 +1,10 @@
+import ProjectSideBar from "../Componentes/ProjectsSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main>
+      <ProjectSideBar/>
+    </main>
   );
 }
 
