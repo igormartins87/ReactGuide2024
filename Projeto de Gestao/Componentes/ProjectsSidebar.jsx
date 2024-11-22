0b228ac1e8f4e682f835b4ffdfcdb9a62098a0d7
igormartins87 <202209131518@alunos.estacio.br>
2024-11-22 17:08:39 -0300

    ajustesTela

--- Projeto de Gestao/Componentes/ProjectsSidebar.jsx
+++ Projeto de Gestao/Componentes/ProjectsSidebar.jsx
@@ -1,8 +1,8 @@
 export default function ProjectSideBar() {
   return (
-    <aside>
+    <aside className="w-1/3 px-8 py-16 bg-slate-800 text-stone-50 md:w-72 rounded-r-xl">
       <h2>Seu Projetos</h2>
-      <div className="w-1/3 px-8 py-16 bg-slate-800 text-stone-50 md:w-72 rounded-r-xl">
+      <div>
         <button>Adicionar Projetos</button>
       </div>
       <ul></ul>
