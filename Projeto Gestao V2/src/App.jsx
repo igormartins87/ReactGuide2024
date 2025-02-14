2050439f72cdd374bd610a9e2205c97260941723
igormartins87 <202209131518@alunos.estacio.br>
2025-02-14 11:55:45 -0300

    Fix:SubindoVersao2

---
+++ Projeto Gestao V2/src/App.jsx
@@ -0,0 +1,13 @@
+import ProjectSideBar from "../Componetes/projetoSideBar";
+
+function App() {
+  return (
+    <>
+      <main className="h-screen my-8">
+        <ProjectSideBar />
+      </main>
+    </>
+  );
+}
+
+export default App;
--- Projeto de Gestao/Componentes/ProjectsSidebar.jsx
+++ Projeto de Gestao/Componentes/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 export default function ProjectSideBar() {
   return (
-    <aside className="w-1/3 px-8 py-16 bg-slate-800 text-stone-50 md:w-72 rounded-r-xl">
+    <aside className="w-1/3 px-8 py-16 bg-stone-800 text-stone-50 md:w-72 rounded-r-xl">
       <h2>Seu Projetos</h2>
       <div>
         <button>Adicionar Projetos</button>
