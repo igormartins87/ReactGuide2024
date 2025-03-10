6ad0202d64369d2b0e551eb40c05489ad34321f4
igormartins87 <202209131518@alunos.estacio.br>
2025-03-10 16:28:40 -0300

    SubindoAlterações

--- props Drillig/src/components/CartModal.jsx
+++ props Drillig/src/components/CartModal.jsx
@@ -1,12 +1,16 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef , useContext } from 'react';
 import { createPortal } from 'react-dom';
 import Cart from './Cart';
+import { CartContext } from '../Loja/shpping-cart-context';
+
 
 const CartModal = forwardRef(function Modal(
   { cartItems, onUpdateCartItemQuantity, title, actions },
   ref
 ) {
   const dialog = useRef();
+
+  const{items,} = useContext(CartContext)
 
   useImperativeHandle(ref, () => {
     return {
