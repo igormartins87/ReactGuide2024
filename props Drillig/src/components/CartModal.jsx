ea52073e6742dd5d74ab0a96b7f4d162339fd238
igormartins87 <202209131518@alunos.estacio.br>
2025-03-12 15:11:06 -0300

    Fix:UseContextAPI

--- props Drillig/src/components/CartModal.jsx
+++ props Drillig/src/components/CartModal.jsx
@@ -1,16 +1,16 @@
-import { forwardRef, useImperativeHandle, useRef , useContext } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import Cart from './Cart';
-import { CartContext } from '../Loja/shpping-cart-context';
+
 
 
 const CartModal = forwardRef(function Modal(
-  { cartItems, onUpdateCartItemQuantity, title, actions },
+  { title, actions },
   ref
 ) {
   const dialog = useRef();
 
-  const{items,} = useContext(CartContext)
+  
 
   useImperativeHandle(ref, () => {
     return {
@@ -23,7 +23,7 @@ const CartModal = forwardRef(function Modal(
   return createPortal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <Cart />
       <form method="dialog" id="modal-actions">
         {actions}
       </form>
