abca9b24b6b898ee03b0386dd74cb28b5f51e94f
igormartins87 <202209131518@alunos.estacio.br>
2025-03-12 17:05:34 -0300

    ajusteLocalização

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -5,11 +5,24 @@ import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
+
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+
+  navigator.geolocation.getCurrentPosition((position) =>{
+    const sortedPlace = sortPlacesByDistance(AVAILABLE_PLACES,
+      position.coords.latitude,
+      position.coords.longitude)
+  });
+
+  setAvailablePlaces(sortedPlace);
+
 
   function handleStartRemovePlace(id) {
     modal.current.open();
